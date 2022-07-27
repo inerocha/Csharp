adefa1dd98a943c23d4535a5346e5b99d4e7e574
Ingrid Rocha <97315541+rochaao@users.noreply.github.com>
2022-07-26 22:07:04 -0300

    Create logUsername.cs

---
+++ logUsername.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneBase
+{
+    public class BaseOne
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Enter username:");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Username is: " + userName);
+        }
+    }
+}
